44b5688e82266c665cf446cdb6ab50415d4d7492
Vladimir Gorbunov <truedaemon@gmail.com>
2013-02-05 23:59:02 +0400

    Change ido-mode settings

--- rc.d/rc-ido.el
+++ rc.d/rc-ido.el
@@ -4,7 +4,9 @@
 
 (require 'ido)
 (ido-mode t)
+(ido-everywhere t)
 (setq ido-enable-flex-matching t)
+(setq ido-enable-tramp-completion nil)
 (setq ido-use-filename-at-point 'guess)
 (setq ido-show-dot-for-dired t)
 
