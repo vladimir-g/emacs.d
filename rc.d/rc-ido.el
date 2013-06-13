936d74ba0848fc56c1f9a091c4d2ee8113b78001
Vladimir Gorbunov <truedaemon@gmail.com>
2013-06-13 18:10:12 +0400

    Open buffer even if it's opened in another frame

--- rc.d/rc-ido.el
+++ rc.d/rc-ido.el
@@ -9,12 +9,20 @@
 (setq ido-enable-tramp-completion nil)
 (setq ido-use-filename-at-point 'guess)
 (setq ido-show-dot-for-dired t)
+;; Show buffer even it's opened somewhere
+(setq ido-default-file-method 'selected-window)
 
 ;; Preventing auto-searches unless called explicitly
 (setq ido-auto-merge-work-directories-length -1)
-(define-key ido-file-dir-completion-map (kbd "C-c C-s")
-  (lambda() 
-    (interactive)
-    (ido-initiate-auto-merge (current-buffer))))
+
+;; Custom keybindings
+(add-hook 'ido-setup-hook 'ido-my-keys)
+
+(defun ido-my-keys ()
+ "Add my keybindings for ido."
+ (define-key ido-file-dir-completion-map (kbd "C-c C-s")
+   (lambda() 
+     (interactive)
+     (ido-initiate-auto-merge (current-buffer)))))
 
 (provide 'rc-ido)
