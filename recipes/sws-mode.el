e7f7e48ab1b034ce45885c792c19872af5df2129
Vladimir Gorbunov <truedaemon@gmail.com>
2014-04-01 23:37:22 +0400

    Add stylus mode and sws-mode with custom recipes

---
+++ recipes/sws-mode.el
@@ -0,0 +1,7 @@
+(:name sws-mode
+       :website "https://github.com/brianc/jade-mode"
+       :description "Significant Whitespace Mode."
+       :type github
+       :pkgname "brianc/jade-mode"
+       :prepare (progn
+                  (autoload 'sws-mode "sws-mode.el")))
