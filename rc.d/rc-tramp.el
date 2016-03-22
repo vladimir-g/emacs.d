34befc64a1d3123294e13877457fb978d54d263e
Vladimir Gorbunov <vsg@suburban.me>
2016-03-22 23:42:02 +0300

    Tramp can use ad-hoc multiple hops now

--- rc.d/rc-tramp.el
+++ rc.d/rc-tramp.el
@@ -6,11 +6,6 @@
 (setq tramp-default-method "scp")
 (setq password-cache-expiry nil)
 
-(add-to-list 'tramp-default-proxies-alist
-             '(nil "\\`rroot\\'" "/ssh:%h:"))
-(add-to-list 'tramp-default-proxies-alist
-             '((regexp-quote (system-name)) nil nil))
-
 (setq tramp-auto-save-directory "/tmp")
 
 (provide 'rc-tramp)
