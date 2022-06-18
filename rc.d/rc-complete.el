803e88886ba1a9941f6abb188a4af1bb1c2827f4
Vladimir Gorbunov <vsg@suburban.me>
2022-06-18 19:55:16 +0300

    Advice find-file to temporarily disable fido with prefix argument

--- rc.d/rc-complete.el
+++ rc.d/rc-complete.el
@@ -7,4 +7,17 @@
 (fido-mode t)
 (fido-vertical-mode t)
 
+(advice-add
+ 'find-file
+ :around
+ (lambda (original-fun &optional prefix)
+   "Add PREFIX argument to call old-style find-file without fido."
+   (interactive "P")
+   (unwind-protect
+       (progn
+         (when prefix
+           (fido-mode -1))
+         (call-interactively original-fun))
+     (when prefix (fido-mode 1)))))
+
 (provide 'rc-complete)
